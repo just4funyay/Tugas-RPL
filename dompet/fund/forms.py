db57c51b6e45f5e469b5e576465de5b1ce4a26fa
just4funyay <118107093+just4funyay@users.noreply.github.com>
2023-06-06 16:35:53 +0700

    Delete dompet directory

--- dompet/fund/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Fundmodel
-class fundForm(forms.ModelForm):
-    class Meta:
-        model = Fundmodel
-        fields = ['kategori','Tanggal','Jumlah','Deskripsi']
-        
-        
-        
